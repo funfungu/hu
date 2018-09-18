a2c4901abb76eed2860804bbbcc0b8c762f5333d
sunnyhqf <sunny_hqf@126.com>
2018-09-18 21:31:42 +0800

    init

--- src/test/java/com/sunny/maven/MyBatisTest.java
+++ src/test/java/com/sunny/maven/MyBatisTest.java
@@ -9,27 +9,39 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MyBatisTest {
 
-    @Test
-    public void testSelect() throws IOException {
+    private SqlSession getSession() throws IOException {
         // 从classpath目录加载配置文件
         InputStream ips = Resources.getResourceAsStream("SqlMapConfig.xml");
         // 获取Builder
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
         // 根据ips配置构建sessionFactory
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(ips);
-
         // sessionFactory打开一个会话， 进行数据库连接操作
         SqlSession session = sqlSessionFactory.openSession();
+        return session;
+    }
 
+    @Test
+    public void testSelect() throws IOException {
+        SqlSession sqlSession = this.getSession();
         // 通过namespace 和 id 调用select SQL语句，设置占位参数，获取查询结果
-        User user = session.selectOne("com.sunny.maven.pojo.User.selectUserById", 22);
-
+        User user = sqlSession.selectOne("com.sunny.maven.pojo.User.selectUserById", 22);
         System.out.println(user.getId() + " : " + user.getUsername() + " : " + user.getAddress());
         // 释放资源
-        session.close();
+        sqlSession.close();
+    }
 
+    @Test
+    public void testSelectByUsername() throws IOException {
+        SqlSession sqlSession = this.getSession();
+        List<User> users = sqlSession.selectList("com.sunny.maven.pojo.User.selectUserByUsername", "小");
+        for (User user : users) {
+            System.out.println(user.getId() + " : " + user.getUsername() + " : " + user.getAddress());
+        }
+        sqlSession.close();
     }
 }
